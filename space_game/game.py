a3e961523a61807b1f6b7032fbf35da3ba494ce9
Артём Нечай <a.nechai@okenit.ru>
2022-03-22 17:29:09 +0300

    split ship and fire logic into separate module

--- space_game/game.py
+++ space_game/game.py
@@ -1,84 +1,10 @@
+"""Main engine of the game."""
+
 import curses
-import asyncio
 import time
-import random
 
-from itertools import cycle
 from stars import get_stars
-
-
-with open('/home/artem/space-game-asyncio/space_game/frames/rocket/rocket_frame_1.txt') as rocket:
-    rocket1 = rocket.read()
-
-with open('/home/artem/space-game-asyncio/space_game/frames/rocket/rocket_frame_2.txt') as rocket:
-    rocket2 = rocket.read()
-
-iterator = cycle([rocket1, rocket2])
-
-TIC_TIMEOUT = 0.01
-BORDER = 1
-
-
-
-
-
-
-
-async def draw_ship(canvas, row, column, symbol):
-    while True:
-        max_row, max_column = canvas.getmaxyx()
-        rows_direction, columns_direction, space_pressed = read_controls(canvas)
-        ship = next(symbol)
-        frame_rows, frame_columns = get_frame_size(ship)
-        if rows_direction > 0:
-            row = min(row + rows_direction, max_row-frame_rows - BORDER)
-        elif rows_direction < 0:
-            row = max(row + rows_direction, 0 + BORDER)
-        if columns_direction > 0:
-            column = min(column + columns_direction, max_column-frame_columns - BORDER)
-        elif columns_direction < 0:
-            column = max(column + columns_direction, 0 + BORDER)
-        # row = min(row + rows_direction, max_row-frame_rows - BORDER)
-        # row = max(row, 0 + BORDER)
-        # column = min(column + columns_direction, max_column-frame_columns - BORDER)
-        # column = max(column, 0 + BORDER)
-        # row = row + rows_direction if rows_direction else row
-        # column = column + columns_direction if columns_direction else column
-        draw_frame(canvas, row, column, ship, negative=False)
-        for _ in range(2):
-            await asyncio.sleep(0)
-        draw_frame(canvas, row, column, ship, negative=True)
-
-
-
-async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
-    """Display animation of gun shot, direction and speed can be specified."""
-
-    row, column = start_row, start_column
-
-    canvas.addstr(round(row), round(column), '*')
-    await asyncio.sleep(0)
-
-    canvas.addstr(round(row), round(column), 'O')
-    await asyncio.sleep(0)
-    canvas.addstr(round(row), round(column), ' ')
-
-    row += rows_speed
-    column += columns_speed
-
-    symbol = '-' if columns_speed else '|'
-
-    rows, columns = canvas.getmaxyx()
-    max_row, max_column = rows - 1, columns - 1
-
-    curses.beep()
-
-    while 0 < row < max_row and 0 < column < max_column:
-        canvas.addstr(round(row), round(column), symbol)
-        await asyncio.sleep(0)
-        canvas.addstr(round(row), round(column), ' ')
-        row += rows_speed
-        column += columns_speed
+from spaceship import animate_spaceship, fire
 
 
 def draw(canvas):
@@ -92,7 +18,7 @@ def draw(canvas):
     courutines = [
         *stars,
         fire(canvas, start_row, start_colunm),
-        draw_ship(canvas, start_row, start_colunm - 2, iterator)
+        draw_ship(canvas, start_row, start_colunm - 2)
     ]
     while True:
         for courutine in courutines.copy():
---
+++ space_game/spaceship.py
@@ -0,0 +1,71 @@
+"""Module for shaceship object and its gunfire."""
+
+import asyncio
+import curses
+
+from settings import FIRE_ROWS_SPEED, FIRE_COLUMNS_SPEED, SPACESHIP_FRAMES, BORDER_THICKNESS
+from utils import read_controls, get_frame_size, draw_frame, cycle_object_frames
+
+
+async def fire(
+        canvas,
+        start_row,
+        start_column,
+        rows_speed=FIRE_ROWS_SPEED,
+        columns_speed=FIRE_COLUMNS_SPEED,
+):
+    """Display animation of gun shot, direction and speed can be specified.
+
+    Args:
+        canvas: playing field.
+        start_row: initianl position of fire object (Y).
+        start_column: initial position of fire object (X).
+        rows_speed: movement within rows through time,
+        columns_speed: movement within columns through time
+    """
+    row, column = start_row, start_column
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+    row += rows_speed
+    column += columns_speed
+    symbol = '-' if columns_speed else '|'
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+    curses.beep()
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
+async def animate_spaceship(canvas, row, column):
+    """Display animation of spaceship, which can be controlled with arrows.
+
+    Args:
+        canvas: playing field.
+        row: position where to display spaceship (Y).
+        column: position where to display spaceship (X).
+    """
+    while True:
+        max_row, max_column = canvas.getmaxyx()
+        rows_direction, columns_direction, space_pressed = read_controls(canvas)
+        ship_frames = cycle_object_frames(SPACESHIP_FRAMES)
+        spaceship = next(ship_frames)
+        frame_rows, frame_columns = get_frame_size(spaceship)
+        if rows_direction > 0:
+            row = min(row + rows_direction, max_row-frame_rows - BORDER_THICKNESS)
+        elif rows_direction < 0:
+            row = max(row + rows_direction, 0 + BORDER_THICKNESS)
+        if columns_direction > 0:
+            column = min(column + columns_direction, max_column-frame_columns - BORDER_THICKNESS)
+        elif columns_direction < 0:
+            column = max(column + columns_direction, 0 + BORDER_THICKNESS)
+        draw_frame(canvas, row, column, spaceship, negative=False)
+        for _ in range(2):
+            await asyncio.sleep(0)
+        draw_frame(canvas, row, column, spaceship, negative=True)
