66d02f56aa83c2fd9c601dd39920877697dd7770
Артём Нечай <a.nechai@okenit.ru>
2022-03-22 17:29:22 +0300

    update settings module

--- space_game/settings.py
+++ space_game/settings.py
@@ -1,8 +1,10 @@
 """Module with constants affecting the game."""
 
+# general
 TIC_TIMEOUT = 0.1
 BORDER_THICKNESS = 1
 
+# stars
 STAR_QUANTITY = 250
 STAR_SYMBOLS = ('+', '*', '.', ':')
 STAR_APPEARANCE_TIMEOUT = 100
@@ -10,9 +12,13 @@ DIM_LIFETIME = 20
 BRIGHT_LIFETIME = 5
 COMMON_LIFETIME = 3
 
+# fire
+FIRE_ROWS_SPEED = -0.3
+FIRE_COLUMNS_SPEED = -0.3
 
-SPACE_KEY_CODE = 32
-LEFT_KEY_CODE = 260
-RIGHT_KEY_CODE = 261
-UP_KEY_CODE = 259
-DOWN_KEY_CODE = 258
+# spaceship
+SPACESHIP_FRAMES = (
+    '/space_game/frames/rocket/rocket_frame_1.txt',
+    '/space_game/frames/rocket/rocket_frame_2.txt',
+)
+INITIAL_SPACESHIP_POSITION_SHIFT = 2
