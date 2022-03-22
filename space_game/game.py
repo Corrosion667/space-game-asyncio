fd46c329baf790f9713d88979c993473ea45352d
Артём Нечай <a.nechai@okenit.ru>
2022-03-22 17:44:57 +0300

    update main engine

--- space_game/game.py
+++ space_game/game.py
@@ -5,20 +5,28 @@ import time
 
 from stars import get_stars
 from spaceship import animate_spaceship, fire
+from settings import TIC_TIMEOUT, INITIAL_SPACESHIP_POSITION_SHIFT, SPACESHIP_FRAMES
+from utils import cycle_object_frames
 
 
 def draw(canvas):
+    """Draw objects on canvas.
+
+    Args:
+        canvas: playing field.
+    """
     canvas.border()
     canvas.nodelay(True)
     curses.curs_set(False)
     stars = get_stars(canvas)
-    max_y, max_x = canvas.getmaxyx()
-    start_row = max_y // 2
-    start_colunm = max_x // 2
+    ship_frames = cycle_object_frames(SPACESHIP_FRAMES)
+    max_row, max_column = canvas.getmaxyx()
+    start_row = max_row // 2
+    start_colunm = max_column // 2 - INITIAL_SPACESHIP_POSITION_SHIFT
     courutines = [
         *stars,
         fire(canvas, start_row, start_colunm),
-        draw_ship(canvas, start_row, start_colunm - 2)
+        animate_spaceship(canvas, start_row, start_colunm, ship_frames)
     ]
     while True:
         for courutine in courutines.copy():
@@ -28,11 +36,13 @@ def draw(canvas):
                 courutines.remove(courutine)
         canvas.border()
         canvas.refresh()
-        time.sleep(0.1)
+        time.sleep(TIC_TIMEOUT)
+
 
 def main():
+    """Execute game as a script."""
     curses.wrapper(draw)
+
 
 if __name__ == '__main__':
     main()
-
